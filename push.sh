0851a21ea33838af4b33acfe6ca062f708ce5a20
Mango0x45 <thomasvoss@live.com>
2021-02-15 20:40:44 +0100

    tabs pls

--- push.sh
+++ push.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env sh
 
+# Easily push changes to both repositories
+
 repos=$(git remote)
 if ! echo $repos | grep 'github' > /dev/null 2>&1; then
-    git remote add github git@github.com:MCBE-Speedrunning/Speedrunning-Rulebook.git
+	git remote add github git@github.com:MCBE-Speedrunning/Speedrunning-Rulebook.git
 fi
 
 if ! echo $repos | grep 'gitea' > /dev/null 2>&1; then
-    git remote add gitea https://git.mcbe.wtf/MCBE-Speedrunning/Speedrunning-Rulebook.git
+	git remote add gitea https://git.mcbe.wtf/MCBE-Speedrunning/Speedrunning-Rulebook.git
 fi
 
 git push gitea
