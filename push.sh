a3b95a214dfaa97907d73f4f7eae002ba8eafd84
Mango0x45 <thomasvoss@live.com>
2021-02-15 20:09:07 +0100

    Add push scrupt

---
+++ push.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+repos=$(git remote)
+if ! echo $repos | grep 'github' > /dev/null 2>&1; then
+    git remote add github git@github.com:MCBE-Speedrunning/Speedrunning-Rulebook.git
+fi
+
+if ! echo $repos | grep 'gitea' > /dev/null 2>&1; then
+    git remote add gitea https://git.mcbe.wtf/MCBE-Speedrunning/Speedrunning-Rulebook.git
+fi
+
+git push remote gitea
+git push remote github
